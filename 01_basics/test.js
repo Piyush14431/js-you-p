a718b44b725cb4df94da0a39bf26a52a357d78b7
Piyush Yadav <109286294+Piyush14431@users.noreply.github.com>
2025-01-05 12:44:22 +0000

    commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
 console.log("Piyush")
+const empID = 14431
+let empEmial = 'piyushyadav14431@gmail.com'
+let empAddress = '17A/1 Ram chandra Mission Road'
+let emppassword = '18877449865'
+let empCity = 'Prayagraj'
+
+catlog.table([empAddress,empCity,empEmial,empID,emppassword,])
