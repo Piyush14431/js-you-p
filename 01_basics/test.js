185e895ea4c8018c610b332382219157d2f457eb
Piyush Yadav <109286294+Piyush14431@users.noreply.github.com>
2025-01-05 13:00:39 +0000

    commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -6,3 +6,8 @@ let emppassword = '18877449865'
 let empCity = 'Prayagraj'
 
 catlog.table([empAddress,empCity,empEmial,empID,emppassword,])
+
+//in present days NodeJS have New Codes, but we have to follow old code to , for this i am using
+// "use strict" <--   treat all code in NodeJs as newer version
+
+// all Documentation      """"https://tc39.es/ecma262/#sec-structured-data""""
