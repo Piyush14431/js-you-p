161557a36d9ab688b531c2108cbac0151cec9a19
Piyush Yadav <109286294+Piyush14431@users.noreply.github.com>
2025-01-10 14:52:57 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,9 @@
+console.log(2 > 1) // so answer in boolean
+console.log(2==1)
+console.log(2 != 1)
+// all this comparision is ok but main problem start with when i compair , string with number
+
+console.log("2" > 1) // result is 2, because node automaticaly detect string is number
+
+// *typeScript* is also a language, and this type of error will not get in typescript,
+// So if i train my self on NodeJS enough, i will avoide typescript
